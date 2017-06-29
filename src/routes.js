f8ba4a565b1f530e51a9ced88e77be3b0559a107
chenyihong <496695119@qq.com>
2017-06-29 16:06:57 +0800

    添加404 500 等路由处理

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,9 @@ import Cascader from './components/ui/Cascader'
 import Table from './components/ui/Table'
 import TimePicker from './components/ui/TimePicker'
 import TreeSelect from './components/ui/TreeSelect'
+import ReactRouters4 from './components/router/ReactRouters4'
+import RRCustom from './components/router/RRCustom'
+
 
 //注释：如果含有子菜单，则children为数组，如果菜单没有子菜单，点击直接可跳转，则children为对象
 const routes = {
@@ -13,7 +16,7 @@ const routes = {
             title:'主页',
             iconType:'home',
             children: {
-                path:'/',
+                path:'/home',
                 component:Home,
                 key:'home',
                 belong:null,
@@ -36,7 +39,50 @@ const routes = {
                     key:'icon',
                     belong:'ui',
                     title:'图标'
+                },{
+                    path:'/ui/timepicker',
+                    component:TimePicker,
+                    key:'timepicker',
+                    belong:'ui',
+                    title:'时间选择'
+                },{
+                    path:'/ui/treeSelect',
+                    component:TreeSelect,
+                    key:'treeSelect',
+                    belong:'ui',
+                    title:'树形选择'
+                },{
+                    path:'/ui/cascader',
+                    component:Cascader,
+                    key:'cascader',
+                    belong:'ui',
+                    title:'联级选择'
+                },{
+                    path:'/ui/table',
+                    component:Table,
+                    key:'table',
+                    belong:'ui',
+                    title:'表格'
                 }]
+        },
+        router:{
+            title:'路由测试',
+            iconType:'home',
+            children: [{
+                path:'/router/base',
+                component:ReactRouters4,
+                key:'router',
+                belong:'router',
+                title:'嵌套路由'
+            },
+            {
+                path:'/router/custom',
+                component:RRCustom,
+                key:'custon',
+                belong:'router',
+                title:'自定义链接'
+            },
+            ]
         }
 }
 
